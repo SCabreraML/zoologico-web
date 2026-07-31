818e68a76f84f697858e081c08ad395aa735a73e
SCabreaML <smcabreral@puce.edu.ec>
2026-07-31 22:47:34 +0000

    Agregar buildspec

---
+++ buildspec.yml.ps1
@@ -0,0 +1,14 @@
+version: 0.2
+
+phases:
+  install:
+    commands:
+      - echo "Iniciando compilación"
+
+  build:
+    commands:
+      - echo "Sitio web estático listo"
+
+artifacts:
+  files:
+    - '**/*'
